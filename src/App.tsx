9c3a36500b6d9b70edc4814e28fb0b4d5306f839
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-08 21:58:42 +0200

    chore:update public index.html

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { About } from './components/About';
 import { Benefits } from './components/Benefits';
 import { JoinUs } from './components/JoinUs';
 import { Contact } from './components/Contact';
-import Maintenance from './components/Maintanance';
+import { Maintenance } from './components/Maintanance';
 
 function App() {
     const isMaintenanceMode = process.env.REACT_APP_MAINTENANCE_MODE === 'true';
 
-  return isMaintenanceMode ? <Maintenance /> : (
+  return isMaintenanceMode ?(
+     <div className="min-h-screen">
+     <Maintenance />
+      </div>): (
 
     <div className="min-h-screen">
     <Layout>
