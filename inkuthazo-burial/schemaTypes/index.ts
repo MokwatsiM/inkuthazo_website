5e5f78818a4cfbed1568f7e0a1b25fc23be16844
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-08 20:27:40 +0200

    task: index.ts for all schematypes

---
+++ inkuthazo-burial/schemaTypes/index.ts
@@ -0,0 +1,11 @@
+import benefits from "./benefits";
+import component from "./component";
+import hero from "./hero";
+import mission from "./mission";
+import navigation from "./navigation";
+import navigationItem from "./navigationItem";
+
+
+
+
+export const schemaTypes = [hero, mission, benefits,navigationItem,navigation,component]
