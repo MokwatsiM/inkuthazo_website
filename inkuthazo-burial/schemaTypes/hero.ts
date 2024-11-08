6e23dce7be408d8854ae0c6fa353b2ecd61d4600
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-08 20:26:52 +0200

    task: hero schema type

---
+++ inkuthazo-burial/schemaTypes/hero.ts
@@ -0,0 +1,36 @@
+export default {
+  name: 'hero',
+  title: 'Hero Section',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'subtitle',
+      title: 'Subtitle (Inhlangano Inamandla)',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'backgroundImage',
+      title: 'Background Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'ctaText',
+      title: 'Call to Action Button Text',
+      type: 'string',
+    },
+  ],
+}
