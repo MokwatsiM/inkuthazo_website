f7f5c93b2d9f14e7189e4706269ef048411ee674
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-08 23:05:01 +0200

    chore: remove studiohost value

--- inkuthazo-burial/sanity.cli.ts
+++ inkuthazo-burial/sanity.cli.ts
@@ -6,7 +6,6 @@ export default defineCliConfig({
     dataset: 'production',
     
   },
-  studioHost: 'inkuthazo-cms',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
